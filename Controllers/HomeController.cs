46a7cd7f61a7c57dbb296d2633406e51497357b3
Adrian José Pereira Santos <adrianjpsantoos@gmail.com>
2022-10-21 20:42:54 -0300

    Criado as Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,35 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Menu()
+    {
+        return View();
+    }
+
+    public IActionResult Reservation()
+    {
+        return View();
+    }
+
+    public IActionResult Gallery()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Blog()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
