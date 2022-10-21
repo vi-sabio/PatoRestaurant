e7afc2bff9c2a2ee6f29b1adcad8c503d48ffa5c
Aline Otília <aos_santos@hotmail.com>
2022-10-20 22:26:45 -0300

    Iniciando Projeto Pato Restaurante

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PatoRestaurant.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
